06dc1d9aa3c6ca3b23d2ea3cf2fe58464e20036c
Michiel Koning <github@michiel.ion3.nl>
2025-12-01 08:08:17 +0100

    Day 1: Secret Entrance (part 1)

--- racket/day01.rkt
+++ racket/day01.rkt
@@ -2,8 +2,21 @@
 
 (require "util.rkt")
 
+(define (read-rotation rot)
+  (let ([dir (substring rot 0 1)]
+        [turns (string->number (substring rot 1))])
+    (if (string=? dir "L") (* -1 turns) turns)))
+
+(define (run-rotations input)
+  (define (turn-dial next acc)
+    (cons (modulo (+ next (car acc)) 100) acc))
+  (let* ([rotations (map read-rotation (lines input))]
+         [stops (foldl turn-dial (list 50) rotations)])
+    stops))
+
 (define (solve1 input)
-  42)
+  (let ([stops (run-rotations input)])
+    (count (lambda (a) (= a 0)) stops)))
 
 (define (go)
   (let ([input (read-input "day01.txt")])
--- racket/util.rkt
+++ racket/util.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
-(provide read-input)
+(provide read-input lines)
 
 (define (read-input file-name)
   (let ([file (open-input-file (string-append-immutable "../input/" file-name))])
     (port->string file)))
+
+(define (lines input)
+  (string-split input "\n"))
